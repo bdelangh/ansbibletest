539755ba448e6bef0509ad67a66e712bf016c280
Bart Delanghe <badelang@microsoft.com>
2020-07-11 13:24:25 +0200

    Updates

--- Readme.md
+++ Readme.md
@@ -1,8 +1,17 @@
 # Ansible Test Scripts
 
-# Usefull Links
+## Usefull Commands
+### Gather Facts
+ansible -i hosts.yml ansible1 -m setup
+
+### Verbose
+ansible -i hosts.ym ansible1 playbook.yml -vvv 
+
+## Usefull Links
 * https://www.tutorialspoint.com/ansible
 *
 *
 * https://docs.ansible.com/ansible/latest/modules/debug_module.html
+
+
 
--- demoScript.ps1
+++ demoScript.ps1
@@ -15,4 +15,8 @@ terraform apply --var-file=singleHANA.json  --auto-approve $tfdir
 terraform destroy --var-file=singleHANA.json --auto-approve $tfdir
 
 
-ansible-playbook -i hosts.yml ~/sap-hana/deploy/ansible/sap_playbook.yml
+ansible-playbook -i hosts.yml sap-hana/deploy/ansible/sap_playbook.yml
+
+#Copy file
+ansible -i hosts  -m copy -a "src = sap-hana/deploy/ansible/test_playbook dest = sap-hana/deploy/ansible/test_playbook"
+ansible abc -m copy -a "src = /etc/yum.conf dest = /tmp/yum.conf"
